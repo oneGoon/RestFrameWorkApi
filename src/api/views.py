6bf7c0b22e24fded19e4779d6bed3f0eacf8d6fa
Alan.Wang1 <alan.wang1@homecredit.cn>
2020-03-06 16:46:17 +0800

    run test

---
+++ src/api/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.serializers import Serializer
+from rest_framework.settings import APISettings
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
+
+class Test(APIView):
+     def get(self, request, *args, **kwargs):
+         return Response('get drf rest ok')
+     def post(self, request, *args, **kwargs):
+         return Response('post drf rest ok')
